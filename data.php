04f4f0b3147c0f4b746cb927c44f5aa377b7fa4e
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2023-05-02 16:34:31 -0300

    Add files via upload

---
+++ data.php
@@ -0,0 +1,16 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cadastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    // if($conexao->connect_errno){
+    //     echo "ERRO";
+    // }
+
+    // else{
+    //     echo "Conexão efetuada";
+    // }
+?>
