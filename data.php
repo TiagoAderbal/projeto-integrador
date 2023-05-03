e62a9551dc94415782a2c94e3be67f053caf5410
TiagoAderbal <108840341+TiagoAderbal@users.noreply.github.com>
2023-05-03 14:44:14 -0300

    Add files via upload

--- data.php
+++ data.php
@@ -6,33 +6,33 @@
 
     $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
 
-    // if(isset($_POST['enviar'])){
-    //     // print_r($_POST['nome']);
-    //     // print_r('<br>');
-    //     // print_r($_POST['email']);
-    //     // print_r('<br>');
-    //     // print_r($_POST['tel']);
-    //     // print_r('<br>');
-    //     // print_r($_POST['datanasc']);
-    //     // print_r('<br>');
-    //     // print_r($_POST['endereco']);
-    //     // print_r('<br>');
-    //     // print_r($_POST['mensagem']);
+    if(isset($_POST['enviar'])){
+        // print_r($_POST['nome']);
+        // print_r('<br>');
+        // print_r($_POST['email']);
+        // print_r('<br>');
+        // print_r($_POST['tel']);
+        // print_r('<br>');
+        // print_r($_POST['datanasc']);
+        // print_r('<br>');
+        // print_r($_POST['endereco']);
+        // print_r('<br>');
+        // print_r($_POST['mensagem']);
 
-    //     include_once('data.php');
+        include_once('data.php');
 
-    //     $nome= $_POST['nome'];
-    //     $email= $_POST['email'];
-    //     $tel= $_POST['tel'];
-    //     $datanasc= $_POST['datanasc'];
-    //     $endereco= $_POST['endereco'];
-    //     $mensagem= $_POST['mensagem'];
+        $nome= $_POST['nome'];
+        $email= $_POST['email'];
+        $tel= $_POST['tel'];
+        $datanasc= $_POST['datanasc'];
+        $endereco= $_POST['endereco'];
+        $mensagem= $_POST['mensagem'];
 
-    //     $result= mysqli_query($conexao, "INSERT INTO projeto(nome, email, tel, datanasc, endereco, mensagem) 
-    //     VALUES ('$nome', '$email', '$tel', '$datanasc', '$endereco', '$mensagem')");
+        $result= mysqli_query($conexao, "INSERT INTO projeto(nome, email, tel, datanasc, endereco, mensagem) 
+        VALUES ('$nome', '$email', '$tel', '$datanasc', '$endereco', '$mensagem')");
 
-    //     echo "Dados salvos com sucesso";
-    // }
+        echo "Dados salvos com sucesso";
+    }
 
     // if($conexao->connect_errno){
     //     echo "ERRO";
